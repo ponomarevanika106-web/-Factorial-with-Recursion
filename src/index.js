3f84c4bfe3d2689da88a95dc635c6fdc80414ce1
Nikol <ponomarevanika106@gmail.com>
2026-04-30 15:16:23 +0300

    added check

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const factorial = (x) => {
-  if (x === 1) {
+  if (typeof x !== "number" || !Number.isFinite(x) || x < 0) {
+    throw new Error("Invalid input");
+  }
+
+  if (x === 0 || x === 1) {
     return 1;
   }
+
   return x * factorial(x - 1);
 };
 
-console.log(factorial(5));
+console.log(factorial(5)); 
